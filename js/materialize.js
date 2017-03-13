e1f851482d171cf7a96eb982d54a6f3c4e6c49e5
DominikKoch <dominik.w.koch@googlemail.com>
2017-03-13 23:23:08 +0100

    Get rid of caret in filter

--- js/materialize.js
+++ js/materialize.js
@@ -3490,7 +3490,7 @@ if (jQuery) {
       // Wrap Elements
       $select.wrap(wrapper);
       // Add Select Display Element
-      var dropdownIcon = $('<span class="caret">&#9660;</span>');
+      var dropdownIcon = $('<span class="caret"></span>');
       if ($select.is(':disabled'))
         dropdownIcon.addClass('disabled');
 
